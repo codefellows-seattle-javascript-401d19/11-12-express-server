7bdcedcaad619618e0e43293c253c504cfe627be
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-11 14:48:12 -0800

    filling in sweet schema in sweet.js

--- lab-shannon/lib/server.js
+++ lab-shannon/lib/server.js
@@ -1,3 +1,13 @@
 'use strict';
 
-const app = require(`express`);
+const logger = require(`./logger`);
+const mongoose = require(`mongoose`);
+const express = require(`express`);
+
+const app = express();
+let serverIsOn = false;
+let httpServer = null;
+
+// because mongoose is older than the ES6 Promise we have to tell mongoose what Promise we're using
+mongoose.Promise = Promise
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
--- lab-shannon/model/sweet.js
+++ lab-shannon/model/sweet.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const mongoose = require(`mongoose`);
+
+// a schema is "a representation of a plan or theory in the form of an outline or model"- we're saying how our sweet will look/behave
+const sweetSchema = mongoose.Scheme({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hasChocolate: {
+    type: Boolean,
+    required: true
+  },
+  temperature: {
+    type: String,
+    required: true
+  }
+});
+
+// A model is a class with which we construct documents (from mongoose docs);
+module.exports = mongoose.model(`sweet`, sweetSchema);
