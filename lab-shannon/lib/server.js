89b14f93a9f7cbd483072a19a691605a09b07f76
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-11 15:59:42 -0800

    set up server.start

--- lab-shannon/lib/server.js
+++ lab-shannon/lib/server.js
@@ -11,3 +11,31 @@ let httpServer = null;
 // because mongoose is older than the ES6 Promise we have to tell mongoose what Promise we're using
 mongoose.Promise = Promise
 mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+
+app.use(require(`../route/sweet-router`));
+app.all(`*`, (request, response) => {
+  logger.log(`info`, `Something went wrong with the request. Sending a 400 status`);
+  return response.sendStatus(400);
+})
+
+//---------------------------------------------------------------------------------------
+const server = module.exports = {};
+
+server.start = () => {
+  return new Promise((resolve, reject) => {
+    if(serverIsOn){
+      logger.log(`info`, `__SERVER_ERROR__ the server is already on`);
+      return reject(new Error(`__SERVER_ERROR__ the server is already on`));
+    }
+    httpServer = app.listen(process.env.PORT, () => {
+      logger.log(`info`, `Turning the server on. Listening on port ${process.env.PORT}`);
+      console.log(`Listening on port ${process.env.PORT}`);
+      serverIsOn = true;
+      return resolve();
+    })
+  });
+};
+
+server.stop = () => {
+
+};
