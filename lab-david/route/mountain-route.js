5bec74fbca3fef846ce00b4b5131820cecf5d384
David Lindahl <davidalindahl@gmail.com>
2017-12-11 15:29:14 -0800

    delete routed added

--- lab-david/route/mountain-route.js
+++ lab-david/route/mountain-route.js
@@ -52,7 +52,7 @@ MOUNTAINROUTER.get('api/notes/:id', (request,response,next) => {
 MOUNTAINROUTER.get('api/notes/', (request,response,next) => {
   LOGGER.log('info', 'GET - processing for a non-ID specfic request');
 
-  MOUNTAIN.find('*')
+  MOUNTAIN.find({})
     .then(MOUNTAIN => {
       if(!MOUNTAIN){
         LOGGER.log('info', 'GET - returning a 404 status code');
@@ -61,6 +61,32 @@ MOUNTAINROUTER.get('api/notes/', (request,response,next) => {
       LOGGER.log('info', 'GET - returning a 200 status code');
       LOGGER.log('info',MOUNTAIN);
       return response.json(MOUNTAIN);
+    }).catch(error => {
+      if(error.message.indexOf('Cast to ObjectId failed') > -1){
+        LOGGER.log('info', 'GET - returning a 404 status code. could not parse the id');
+        return response.sendStatus(404);
+      }
+      LOGGER.log('error', 'GET - returning a 500 code');
+      LOGGER.log('error', error);
+      return response.sendStatus(500);
+    });
+});
+
+MOUNTAINROUTER.delete('api/notes/:id', (request,response,next) => {
+  LOGGER.log('info', 'DELETE - processing a delete request for a specific id');
+
+  MOUNTAIN.findById(request.params.id)
+    .then(MOUNTAIN => {
+      if(!MOUNTAIN){
+        LOGGER.log('info', 'GET - returning a 404 status code');
+        return response.sendStatus(404);
+      }
+      LOGGER.log('info', 'GET - returning a 200 status code');
+      LOGGER.log('info',MOUNTAIN);
+
+      response.json(MOUNTAIN).delete();
+      return response.json(MOUNTAIN);
+
     }).catch(error => {
       if(error.message.indexOf('Cast to ObjectId failed') > -1){
         LOGGER.log('info', 'GET - returning a 404 status code. could not parse the id');
