366ac6ebf029a472be9351f284fd547019c62b77
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-11 16:05:18 -0800

    all tests from class passing

--- lab-cameron/__test__/server.test.js
+++ lab-cameron/__test__/server.test.js
@@ -35,5 +35,47 @@ describe('/api/users', () => {
           expect(response.status).toEqual(200);
         });
     });
+
+    test('should respond with a 400 code if we send an incomplete user', () => {
+      const userToPost = {
+        name: faker.lorem.words(10),
+      };
+      return superagent.post(`${apiURL}`)
+        .send(userToPost)
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(400);
+        });
+    });
+  });
+
+  describe('GET /api/users', () => {
+    test('should respond with a 200 status code if there is no error', () => {
+      let userToTest = null;
+
+      userMockCreate()
+        .then(user => {
+          userToTest = user;
+          return superagent.get(`${apiURL}/${user._id}`);
+        })
+        .then(response => {
+          console.log(response.body);
+          expect(response.status).toEqual(200);
+
+          expect(response.body._id).toEqual(userToTest._id.toString());
+          expect(response.body.timestamp).toBeTruthy();
+
+          expect(response.body.name).toEqual(userToTest.name);
+          expect(response.body.description).toEqual(userToTest.description);
+        });
+    });
+
+    test('should respond with 404 status code if the id is incorrect', () => {
+      return superagent.get(`${apiURL}/invalidId`)
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(404);
+        });
+    });
   });
 });
