ae7b3fe5eac4f7457abbaf81a5bb312a4c505339
Robert Reed <robert.mc.reed@gmail.com>
2017-12-11 16:08:44 -0800

    server test written

---
+++ lab-rob/__test__/server.test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+require('dotenv').config();
+const superagent = require('superagent');
+const Bike = require('../model/bike');
+const server = require('../lib/server');
+const __API_URL__ = `http://localhost:${process.env.PORT}/api/bikes`;
+
+const createBike = () => {
+  return new Bike({
+    make: 'Buell',
+    model: 'XB12s',
+    year: 2004,
+    displacement: 1203,
+  }).save();
+};
+
+describe('/api/bikes', () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+  afterEach(() => Bike.remove({}));
+
+  describe('POST /api/bikes', () => {
+    test('should respond with a note and 200 status code if there is no error.', () => {
+      let bikeToPost = {
+        make: 'Buell',
+        model: 'XB12s',
+        year: 2004,
+        displacement: 1203,
+      };
+
+      return superagent.post(__API_URL__)
+        .send(bikeToPost)
+        .then(response => {
+          expect(response.status).toEqual(200);
+          expect(response.body._id).toBeTruthy();
+          expect(response.body.timestamp).toBeTruthy();
+          expect(response.body.make).toEqual('Buell');
+          expect(response.body.model).toEqual('XB12s');
+          expect(response.body.year).toEqual(2004);
+          expect(response.body.displacement).toEqual(1203);
+        });
+    });
+  });
+});
