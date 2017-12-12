cddee6fe290e95b5b833a15a3a515561d93af34b
Andrew Bloom <blooma116@gmail.com>
2017-12-12 15:49:03 -0800

    middleware set up

--- lab-andrew/route/cat-router.js
+++ lab-andrew/route/cat-router.js
@@ -3,51 +3,38 @@
 const Cat = require('../model/cat');
 const {Router} = require('express');
 const logger = require('../lib/logger');
+const httpErrors = require('http-errors');
 const jsonParser = require('body-parser').json();
 const catRouter = module.exports = new Router();
 
-catRouter.post('/api/cats', jsonParser, (request, response) => {
+catRouter.post('/api/cats', jsonParser, (request, response, next) => {
   logger.log('info', 'POST - processing new post request');
 
   if (!request.body.name || !request.body.says) {
     logger.log('info', 'POST - responding with a 400');
-    return response.sendStatus(400);
+    return next(httpErrors(400, 'body and content are required'));
   }
 
   return new Cat(request.body).save()
     .then(cat => response.json(cat))
-    .catch(error => {
-      logger.log('error', '__SERVER_ERROR__');
-      logger.log('error', error);
-
-      return response.sendStatus(500);
-    });
+    .catch(next);
 });
 
-catRouter.get('/api/cats/:id', (request, response) => {
+catRouter.get('/api/cats/:id', (request, response, next) => {
   logger.log('info', 'GET - processing a new get request');
 
   return Cat.findById(request.params.id)
     .then(cat => {
       if (!cat){
-        logger.log('info', 'GET - Returning a 404 status code');
-        return response.sendStatus(404);
+        throw httpErrors(404, 'cat not found');
       }
       logger.log('info', 'GET - Returning a 200 status code');
       logger.log('info', cat);
       return response.json(cat);
-    }).catch(error => {
-      if (error.message.indexOf('Cast to ObjectId failed') > -1){
-        logger.log('info', 'GET - Returning a 404 status code. Could not parse the id');
-        return response.sendStatus(404);
-      }
-      logger.log('error', 'GET - Returning a 500 code');
-      logger.log('error', error);
-      return response.sendStatus(500);
-    });
+    }).catch(next);
 });
 
-catRouter.get('/api/cats', (request, response) => {
+catRouter.get('/api/cats', (request, response, next) => {
   logger.log('info', 'GET - processing a new get request');
 
   return Cat.find({})
@@ -55,44 +42,36 @@ catRouter.get('/api/cats', (request, response) => {
       logger.log('info', 'GET - Returning a 200 status code');
       return response.json(cats);
     })
-    .catch(error => logger.log('error', error));
+    .catch(error => {
+      return next(httpErrors(500, `error with retrieving from db: ${error}`));
+    });
 });
 
-catRouter.get('/api/cats/:id', (request, response) => {
+catRouter.get('/api/cats/:id', (request, response, next) => {
   logger.log('info', 'GET - processing a new get request');
 
   return Cat.findById(request.params.id)
     .then(cat => {
       if (!cat){
-        logger.log('info', 'GET - Returning a 404 status code');
-        return response.sendStatus(404);
+        throw httpErrors(404, 'cat not found');
       }
       logger.log('info', 'GET - Returning a 200 status code');
       logger.log('info', cat);
       return response.json(cat);
-    }).catch(error => {
-      if (error.message.indexOf('Cast to ObjectId failed') > -1){
-        logger.log('info', 'GET - Returning a 404 status code. Could not parse the id');
-        return response.sendStatus(404);
-      }
-      logger.log('error', 'GET - Returning a 500 code');
-      logger.log('error', error);
-      return response.sendStatus(500);
-    });
+    })
+    .catch(next);
 });
 
-catRouter.put('/api/cats/:id', jsonParser, (request, response) => {
+catRouter.put('/api/cats/:id', jsonParser, (request, response, next) => {
   logger.log('info', 'PUT - processing a new put request');
 
   return Cat.findById(request.params.id)
     .then(cat => {
       if (!request.body.name || !request.body.says) {
-        logger.log('info', 'POST - responding with a 400');
-        return response.sendStatus(400);
+        throw httpErrors(400, 'body and content are required');
       }
       if (!cat){
-        logger.log('info', 'PUT - Returning a 404 status code');
-        return response.sendStatus(404);
+        throw httpErrors(404, 'cat not found');
       }
       cat.set({
         name: `${request.body.name}`,
@@ -103,43 +82,25 @@ catRouter.put('/api/cats/:id', jsonParser, (request, response) => {
       return cat.save()
         .then(updatedCat => response.json(updatedCat))
         .catch(error => {
-          logger.log('error', 'PUT - Returning a 500 code');
-          logger.log('error', error);
-          return response.sendStatus(500);
+          return next(httpErrors(500, `error with saving to db: ${error}`));
         });
-    }).catch(error => {
-      if (error.message.indexOf('Cast to ObjectId failed') > -1){
-        logger.log('info', 'PUT - Returning a 404 status code. Could not parse the id');
-        return response.sendStatus(404);
-      }
-      logger.log('error', 'PUT - Returning a 500 code');
-      logger.log('error', error);
-      return response.sendStatus(500);
-    });
+    })
+    .catch(next);
 });
 
-catRouter.delete('/api/cats', (request, response) => {
-  logger.log('info', 'You must pass an ID to DELETE');
-  return response.sendStatus(400);
+catRouter.delete('/api/cats', (request, response, next) => {
+  return next(httpErrors(400, 'You must pass an ID to DELETE'));
 });
 
-catRouter.delete('/api/cats/:id', (request, response) => {
+catRouter.delete('/api/cats/:id', (request, response, next) => {
   return Cat.findByIdAndRemove(request.params.id)
     .then(cat => {
       if (!cat){
-        logger.log('info', 'DELETE - Returning a 404 status code');
-        return response.sendStatus(404);
+        throw httpErrors(404, 'cat not found');
       }
       logger.log('info', 'DELETE - Returning a 200 status code');
       logger.log('info', cat);
       return response.sendStatus(204);
-    }).catch(error => {
-      if (error.message.indexOf('Cast to ObjectId failed') > -1){
-        logger.log('info', 'DELETE - Returning a 404 status code. Could not parse the id');
-        return response.sendStatus(404);
-      }
-      logger.log('error', 'DELETE - Returning a 500 code');
-      logger.log('error', error);
-      return response.sendStatus(500);
-    });
+    })
+    .catch(next);
 });
