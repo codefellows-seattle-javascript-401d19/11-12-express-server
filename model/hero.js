85372f5651f8908ee48859b4c6a873779b2c2934
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-11 17:42:07 -0800

    adding hero.js and post/get methods

--- model/hero.js
+++ model/hero.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const heroSchema = mongoose.Schema({
+  name : {
+    type : String,
+    required : true,
+    unique : true,
+  },
+  description : {
+    type : String,
+    required : true,
+    minlength : 10,
+  },
+  timestamp : {
+    type : Date,
+    default : () => new Date(),
+  },
+});
+
+module.exports = mongoose.model('hero', heroSchema);
