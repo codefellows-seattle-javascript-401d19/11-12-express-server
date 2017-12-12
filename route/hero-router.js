de287d801d9e8e450dd405b7fc1b1bb81601b8a2
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-11 18:28:01 -0800

    adding DELETE method

--- route/hero-router.js
+++ route/hero-router.js
@@ -48,3 +48,25 @@ heroRouter.get('/api/heroes/:id', (request,response,next) => {
       return response.sendStatus(500);
     });
 });
+
+heroRouter.delete('/api/heroes/:id', (request,response,next) => {
+  logger.log('info', 'DELETE - processing a request');
+
+  Hero.findById(request.params.id)
+    .then(hero => {
+      if(!hero){
+        logger.log('info', 'ERROR__DELETE - Returning a 400 status code');
+        return response.sendStatus(400);
+      }
+      logger.log('info', 'SUCCESSFUL__DELETE - Returning a 204 status code');
+      logger.log('info',hero);
+    }).catch(error => {
+      if(error.message.indexOf('Cast to ObjectId failed') > -1){
+        logger.log('info', 'ERROR__DELETE - Returning a 404 status code. Could not parse id');
+        return response.sendStatus(404);
+      }
+      logger.log('error', 'ERROR__DELETE - Returning a 500 status code');
+      logger.log('error',error);
+      return response.sendStatus(500);
+    });
+});
