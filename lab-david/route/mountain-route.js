d8ddf088834206b013cb23cbc43f21f6426c08d1
David Lindahl <davidalindahl@gmail.com>
2017-12-12 14:49:25 -0800

    updates to the mountain route

--- lab-david/route/mountain-route.js
+++ lab-david/route/mountain-route.js
@@ -3,59 +3,50 @@
 const {Router} = require('express');
 const jsonParser = require('body-parser').json();
 
-const mountain = require('../model/mountain');
+const Mountain = require('../model/mountain');
 const logger = require('../lib/logger');
+const httpErrors = require('http-errors');
 
 const mountainRoute = module.exports = new Router();
 
-mountainRoute.post('/api/mountains', jsonParser, (request,response) => {
-  logger.log('info', 'POST - processing that request');  
-
+mountainRoute.post('/api/mountains', jsonParser, (request,response,next) => {
   if(!request.body.name || !request.body.state || !request.body.range) {
-    logger.log('info', 'POST - responding with a 400 code');
-    return response.sendStatus(400);
+    return next(httpErrors(400, 'body and content are required' ));
   }
 
-  return new mountain(request.body).save()
+  return new Mountain(request.body).save()
     .then(mountain => {
-      logger.log('info', 'returning with a 200 status and a mountain');
       return response.json(mountain);
     })
-    .catch(error => {
-      logger.log('error', '--->SERVER_ERROR<---');
-      logger.log('error', error);
-
-      return response.sendStatus(500);
-    });
+    .catch(next);
 });
 
-mountainRoute.get('/api/mountains/:id', (request,response) => {
-  logger.log('info', 'GET - processing a request for a specific id');
-
-  mountain.findById(request.params.id)
+mountainRoute.get('/api/mountains/:id', (request,response,next) => {
+  return Mountain.findById(request.params.id)
     .then(mountain => {
       if(!mountain){
-        logger.log('info', 'GET - returning a 404 status code');
-        return response.sendStatus(404);
+        throw httpErrors(404,`note not found`);
       }
       logger.log('info', 'GET - returning a 200 status code');
-      logger.log('info',mountain);
       return response.json(mountain);
-    }).catch(error => {
-      if(error.message.toLowerCase().includes('cast to objectid failed')){
-        logger.log('info', 'GET - returning a 404 status code. could not parse the id');
-        return response.sendStatus(404);
+    }).catch(next);
+});
+
+mountainRoute.delete('/api/mountains/:id',(request,response,next) => {
+  return Mountain.findByIdAndRemove(request.params.id)
+    .then(mountain => {
+      if(!mountain){
+        throw httpErrors(404,`mountain was not found. go back to start, do not collect $200.`);
       }
-      logger.log('error', 'GET - returning a 500 code');
-      logger.log('error', error);
-      return response.sendStatus(500);
-    });
+      logger.log('info', `GET - returning a 204 status code`);
+      return response.sendStatus(204);
+    }).catch(next);
 });
 
 mountainRoute.get('/api/mountains/', (request,response) => {
   logger.log('info', 'GET - processing for a non-ID specific request');
 
-  mountain.find({})
+  Mountain.find({})
     .then(mountain => {
       if(!mountain){
         logger.log('info', 'GET - returning a 404 status code');
@@ -78,7 +69,7 @@ mountainRoute.get('/api/mountains/', (request,response) => {
 mountainRoute.delete('/api/mountains/:id', (request,response) => {
   logger.log('info', 'DELETE - processing a delete request for a specific id');
 
-  mountain.findById(request.params.id)
+  Mountain.findById(request.params.id)
     .then(mountain => {
       if(!mountain){
         logger.log('info', 'DELETE - returning a 404 status code');
@@ -99,4 +90,17 @@ mountainRoute.delete('/api/mountains/:id', (request,response) => {
       logger.log('error', error);
       return response.sendStatus(500);
     });
+});
+
+mountainRoute.put('/api/mountains/:id', jsonParser,(request,response,next) => {
+  let options = {runValidators: true, new : true};
+
+  return Mountain.findByIdAndUpdate(request.params.id,request.body,options)
+    .then(mountain => {
+      if(!mountain){
+        throw httpErrors(404, 'mountain was not found');
+      }
+      logger.log('info', 'GET - returning a 200 status code');
+      return response.json(mountain);
+    }).catch(next);
 });
