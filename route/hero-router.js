089de4c824170c7f3009ff18068137ae1eee5631
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-12 20:37:35 -0800

    updating methods and adding PUT

--- route/hero-router.js
+++ route/hero-router.js
@@ -5,65 +5,54 @@ const jsonParser = require('body-parser').json();
 
 const Hero = require('../model/hero');
 const logger = require('../lib/logger');
+const httpErrors = require('http-errors');
 
 const heroRouter = module.exports = new Router();
 
+// POST METHOD
 heroRouter.post('/api/heroes', jsonParser, (request, response, next) => {
-  logger.log('info', 'POST - processing a request');
-
   if(!request.body.name || !request.body.description) {
-    logger.log('info', 'POST - responding with a 400 code');
-    return response.sendStatus(400);
+    return next(httpErrors(400, 'body and content are required'));
   }
-
   return new Hero(request.body).save()
     .then(hero => response.json(hero))
-    .catch(error => {
-      logger.log('error', '__SERVER_ERROR__');
-      logger.log('error', error);
-
-      return response.sendStatus(500);
-    });
+    .catch(next);
 });
 
-
+// GET METHOD
 heroRouter.get('/api/heroes/:id', (request,response,next) => {
-  logger.log('info', 'GET - processing a request');
-
   return Hero.findById(request.params.id)
     .then(hero => {
       if(!hero){
-        logger.log('info', 'GET - Returning a 404 status code');
-        return response.sendStatus(404);
+        throw httpErrors(404, 'hero not found');
       }
       logger.log('info', 'GET - Returning a 200 status code');
-    }).catch(error => {
-      if(error.message.indexOf('Cast to ObjectId failed') > -1){
-        logger.log('info', 'GET - Returning a 404 status code. Could not parse id');
-        return response.sendStatus(404);
-      }
-      logger.log('error', 'GET - Returning a 500 status code');
-      return response.sendStatus(500);
-    });
+      return response.json(hero);
+    }).catch(next);
 });
 
+// DELETE METHOD
 heroRouter.delete('/api/heroes/:id', (request,response,next) => {
-  logger.log('info', 'DELETE - processing a request');
-  console.log('inside DELETE');
-
-  return Hero.findById(request.params.id)
+  return Hero.findByIdAndDelete(request.params.id)
     .then(hero => {
       if(!hero){
-        logger.log('info', 'ERROR__DELETE - Returning a 400 status code');
-        return response.sendStatus(400);
+        throw httpErrors(404, 'hero not found');
       }
-      logger.log('info', 'SUCCESSFUL__DELETE - Returning a 204 status code');
-    }).catch(error => {
-      if(error.message.indexOf('Cast to ObjectId failed') > -1){
-        logger.log('info', 'ERROR__DELETE - Returning a 404 status code. Could not parse id');
-        return response.sendStatus(404);
+      logger.log('info', 'GET - Returning a 204 status code');
+      return response.sendStatus(204);
+    }).catch(next);
+});
+
+// PUT METHOD
+heroRouter.put('/api/heroes/:id',jsonParser, (request,response,next) => {
+  let options = {runValidators : true, new : true};
+
+  return Hero.findByIdAndUpdate(request.params.id, request.body, options)
+    .then(hero => {
+      if(!hero){
+        throw httpErrors(404, 'hero not found');
       }
-      logger.log('info', 'ERROR__DELETE - Returning a 500 status code');
-      return response.sendStatus(500);
-    });
+      logger.log('info', 'GET - Returning a 204 status code');
+      return response.json(hero);
+    }).catch(next);
 });
